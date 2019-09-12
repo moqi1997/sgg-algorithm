c41c45f4e545604c1fb8d071bb9f88dc0b5e396e
moqi <ABC@QQ.COM>
2019-09-12 21:20:41 +0800

    稀疏数组与二维数组转换的思路

---
+++ src/main/java/ch01/SparseArray.java
@@ -0,0 +1,62 @@
+package ch01;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个原始大的二维数组11 * 11
+        //0: 表示没有棋子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[2][7] = 2;
+        chessArr1[7][3] = 2;
+        for (int[] row : chessArr1) {
+            for (int i : row) {
+                System.out.printf("%d\t", i);
+            }
+            System.out.println();
+        }
+        //1.先遍历二维数组 得到非0数据的个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        int sparseArr[][] = new int[sum + 1][3];
+        ///给稀疏数组赋值
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+
+        int count = 0;//用于记录是第几个非0数据
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                   sparseArr[count][0] = i;
+                   sparseArr[count][1] = j;
+                   sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("得到的稀疏数组");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n",sparseArr[i][0],sparseArr[i][1],sparseArr[i][2]);
+        }
+
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1] = 11];
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        System.out.println("恢复后的二维数组");
+        for (int[] row : chessArr1) {
+            for (int i : row) {
+                System.out.printf("%d\t", i);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/ch01/TEst.java
+++
@@ -1,4 +0,0 @@
-package ch01;
-
-public class TEst {
-}
